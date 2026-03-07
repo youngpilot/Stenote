a12596990917756e5687f3520eb82e678c5fbe38
youngpilot <15856506+youngpilot@users.noreply.github.com>
2026-03-07 19:22:18 +0100

    Two-tone recording icon: red mic body, system-colored stand
    Only the pill-shaped mic body turns red when recording. The U-shaped
    stand stays in the system menu bar color (white on dark, black on light)
    using a drawing handler that adapts to appearance changes.

--- Talkman/App/TalkmanApp.swift
+++ Talkman/App/TalkmanApp.swift
@@ -14,26 +14,37 @@ struct TalkmanApp: App {
         .menuBarExtraStyle(.window)
     }
 
-    private static let solarMicBoldSVG = """
-    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path fill="{color}" d="M12 2a5.75 5.75 0 0 0-5.75 5.75v3a5.75 5.75 0 0 0 11.452.75H13a.75.75 0 0 1 0-1.5h4.75V8.5H13A.75.75 0 0 1 13 7h4.701A5.75 5.75 0 0 0 12 2"/><path fill="{color}" fill-rule="evenodd" d="M4 9a.75.75 0 0 1 .75.75v1a7.25 7.25 0 1 0 14.5 0v-1a.75.75 0 0 1 1.5 0v1a8.75 8.75 0 0 1-8 8.718v2.282a.75.75 0 0 1-1.5 0v-2.282a8.75 8.75 0 0 1-8-8.718v-1A.75.75 0 0 1 4 9" clip-rule="evenodd"/></svg>
+    private static let solarMicSVGTemplate = """
+    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path fill="{topColor}" d="M12 2a5.75 5.75 0 0 0-5.75 5.75v3a5.75 5.75 0 0 0 11.452.75H13a.75.75 0 0 1 0-1.5h4.75V8.5H13A.75.75 0 0 1 13 7h4.701A5.75 5.75 0 0 0 12 2"/><path fill="{bottomColor}" fill-rule="evenodd" d="M4 9a.75.75 0 0 1 .75.75v1a7.25 7.25 0 1 0 14.5 0v-1a.75.75 0 0 1 1.5 0v1a8.75 8.75 0 0 1-8 8.718v2.282a.75.75 0 0 1-1.5 0v-2.282a8.75 8.75 0 0 1-8-8.718v-1A.75.75 0 0 1 4 9" clip-rule="evenodd"/></svg>
     """
 
+    private static func makeSVGImage(topColor: String, bottomColor: String) -> NSImage? {
+        let svg = solarMicSVGTemplate
+            .replacingOccurrences(of: "{topColor}", with: topColor)
+            .replacingOccurrences(of: "{bottomColor}", with: bottomColor)
+        guard let data = svg.data(using: .utf8), let image = NSImage(data: data) else { return nil }
+        image.size = NSSize(width: 18, height: 18)
+        return image
+    }
+
     static let micIdleIcon: NSImage = {
-        let svg = solarMicBoldSVG.replacingOccurrences(of: "{color}", with: "black")
-        guard let data = svg.data(using: .utf8), let image = NSImage(data: data) else {
+        guard let image = makeSVGImage(topColor: "black", bottomColor: "black") else {
             return NSImage(systemSymbolName: "mic.fill", accessibilityDescription: "Microphone")!
         }
-        image.size = NSSize(width: 18, height: 18)
         image.isTemplate = true
         return image
     }()
 
     static let micRecordingIcon: NSImage = {
-        let svg = solarMicBoldSVG.replacingOccurrences(of: "{color}", with: "#E04848")
-        guard let data = svg.data(using: .utf8), let image = NSImage(data: data) else {
-            return NSImage(systemSymbolName: "mic.fill", accessibilityDescription: "Microphone")!
+        let size = NSSize(width: 18, height: 18)
+        let image = NSImage(size: size, flipped: false) { rect in
+            let isDark = NSAppearance.currentDrawing().bestMatch(from: [.darkAqua, .aqua]) == .darkAqua
+            let standColor = isDark ? "white" : "black"
+            if let svgImage = makeSVGImage(topColor: "#E04848", bottomColor: standColor) {
+                svgImage.draw(in: rect)
+            }
+            return true
         }
-        image.size = NSSize(width: 18, height: 18)
         image.isTemplate = false
         return image
     }()
